713abb3339a528b8add88ac614a17cd8ae2b5254
Lennylot <jarredjlopez@gmail.com>
2022-10-25 21:37:11 -0500

    Update

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import random
-board = [[0,0,0],
-         [0,0,0],
-         [0,0,0]]
+
+board = [[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0]]
 
 
 def user_input():
-    row_choice = int(input("Enter a number to choose row(0-2): "))          
-    column_choice = int(input("Enter a number to choose column(0-2): "))
-    return row_choice,column_choice
+    try:
+        row_choice = int(input("Enter a number to choose row(0-2): "))
+        column_choice = int(input("Enter a number to choose column(0-2): "))
+        return row_choice, column_choice
+    except
+
 
 def player_move():
     player_piece = 1
-    row_choice,column_choice = user_input()
+    row_choice, column_choice = user_input()
     board[row_choice][column_choice] = player_piece
-    return board
 
-    
 
 
 def display_board():
@@ -24,59 +26,42 @@ def display_board():
 
 
 def win_condition():
-    #horizontal
+    # horizontal
     for rows in board:
-        if rows.count(rows[0]) == len(rows) and rows[0] != 0:
+        if rows.count(rows[1]) == len(rows) and rows[2] != 0:
             print(f"Player {rows[0]} is the winner by horizontal ")
-
-    #vertical
+            return True
+    # vertical
     check_vertical_win = []
     for i in range(len(board)):
         for x in board:
             check_vertical_win.append(x[i])
-        if check_vertical_win.count(check_vertical_win[0]) == len(check_vertical_win) and check_vertical_win[0] != 0:
+        if check_vertical_win.count(check_vertical_win[1]) == len(check_vertical_win) and check_vertical_win[2] != 0:
             print(f"Player {rows[0]} is the winner by vertical ")
-
-    #diagonal
+            return True
+    # diagonal
     check_diagonal_win = []
     for d in range(len(board)):
         check_diagonal_win.append(board[d][d])
-    if check_diagonal_win.count(check_diagonal_win[0]) == len(check_diagonal_win) and check_diagonal_win[0] != 0:
-            print(f"Player {rows[0]} is the winner by \ ")
-
-    #reverse diagonal
+    if check_diagonal_win.count(check_diagonal_win[1]) == len(check_diagonal_win) and check_diagonal_win[2] != 0:
+        print(f"Player {rows[0]} is the winner by \ ")
+        return True
+    # reverse diagonal
     reversed_cols = list(reversed(range(len(board))))
     reversed_rows = range(len(board))
     check_reversed_diagonal_win = []
-    for reversed_cols,reversed_rows, in zip(reversed_cols,reversed_rows):
+    for reversed_cols, reversed_rows, in zip(reversed_cols, reversed_rows):
         check_reversed_diagonal_win.append(board[reversed_cols][reversed_rows])
-    if check_reversed_diagonal_win.count(check_reversed_diagonal_win[0]) == len(check_reversed_diagonal_win) and check_reversed_diagonal_win[0] != 0:
-            print(f"Player {rows[0]} is the winner by / ")
-
+    if check_reversed_diagonal_win.count(check_reversed_diagonal_win[1]) == len(check_reversed_diagonal_win) and \
+            check_reversed_diagonal_win[2] != 0:
+        print(f"Player {rows[0]} is the winner by / ")
+        return True
 
 def cpu_move():
     cpu_piece = 2
-    random_row = random.randint(0,2)
+    random_row = random.randint(0, 2)
     random_column = random.randint(0, 2)
     board[random_row][random_column] = cpu_piece
-    return board
-    
-
-
-
-
-
-
-
-
-play = True
-while play:
-    game_won = False
-    while not game_won:
-        display_board()
-        player_move()
-        cpu_move()
-        win_condition()
 
 
 
@@ -86,23 +71,12 @@ while play:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+while True:
+    display_board()
+    player_move()
+    cpu_move()
+    if win_condition():
+       break
 
 
 
