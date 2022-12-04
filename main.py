5971caed46b1f3c3e8b7d65669087c6f674e63db
Lennylot <jarredjlopez@gmail.com>
2022-12-03 20:10:23 -0600

    Update

--- main.py
+++ main.py
@@ -1,91 +1,110 @@
 import random
-
-board = [[0, 0, 0],
-         [0, 0, 0],
-         [0, 0, 0]]
-
-
-def user_input():
-    row_choice = int(input("Enter a number to choose row(0-2): "))
-    column_choice = int(input("Enter a number to choose column(0-2): "))
-    return row_choice, column_choice
+num_row = 6
+num_col = 7
+print("Welcome to my Connect 4 game")
+print("The player is number 1 and the computer is number 2")
 
 
+board = [[0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0]]
 
 
-def player_move():
-    try:
-        player_piece = 1
-        row_choice, column_choice = user_input()
-        board[row_choice][column_choice] = player_piece
-    except:
-        print("Make sure to input numbers from (0-2) because the game won't work")
+
+
+
+
+
+
+
 
 
 
 def display_board():
-    print("   0  1  2")
+    print("   0  1  2  3  4  5  6  ")
     for count, row in enumerate(board):
         print(count,row)
 
 
-def win_condition():
-    # horizontal
-    for rows in board:
-        if rows.count(rows[1]) == len(rows) and rows[2] != 0:
-            print(f"Player {rows[0]} is the winner by horizontal ")
-            return True
-    # vertical
-    check_vertical_win = []
-    for i in range(len(board)):
-        for x in board:
-            check_vertical_win.append(x[i])
-        if check_vertical_win.count(check_vertical_win[1]) == len(check_vertical_win) and check_vertical_win[2] != 0:
-            print(f"Player {rows[0]} is the winner by vertical ")
-            return True
-    # diagonal
-    check_diagonal_win = []
-    for d in range(len(board)):
-        check_diagonal_win.append(board[d][d])
-    if check_diagonal_win.count(check_diagonal_win[1]) == len(check_diagonal_win) and check_diagonal_win[2] != 0:
-        print(f"Player {rows[0]} is the winner by \ ")
-        return True
-    # reverse diagonal
-    reversed_cols = list(reversed(range(len(board))))
-    reversed_rows = range(len(board))
-    check_reversed_diagonal_win = []
-    for reversed_cols, reversed_rows, in zip(reversed_cols, reversed_rows):
-        check_reversed_diagonal_win.append(board[reversed_cols][reversed_rows])
-    if check_reversed_diagonal_win.count(check_reversed_diagonal_win[1]) == len(check_reversed_diagonal_win) and \
-            check_reversed_diagonal_win[2] != 0:
-        print(f"Player {rows[0]} is the winner by / ")
-        return True
+def stacking(col, player):
+    col=col-1
+    for rows in range(num_row-1,-1,-1):
+        if board[rows][col] == 0:
+            print("legal move")
+            board[rows][col] = player
+            break
 
+def horizwin():
+    for row in range(0,num_row):
+        for col in range(0,4):
+            if board[row][col] > 0:
+             if board[row][col] == board[row][col+1] == \
+                board[row][col+2] == board[row][col+3]:
+                print("Player", board[row][col], "won!")
+                return True
+
+def vertwin():
+    for row in range(0, 3):
+        for col in range(0, num_col):
+            if board[row][col] > 0:
+                if board[row][col] == board[row+1][col] == \
+                board[row+2][col] == board[row+3][col]:
+                    print("Player", board[row][col], "won!")
+                    return True
+
+def diagwin():
+    #this is going up and to the right
+    for row in range(0,2):
+        for col in range(0,7):
+            if board[row][col] > 0:
+                if board[row][col] == board[row-1][col+1] == \
+                    board[row-2][col+2] == board[row-3][col+3]:
+                    print("Player",board[row][col], "won!")
+                    return True
+
+    #going down and to the right
+    for row in range(0,3):
+        for col in range(0,4):
+            if board[row][col] > 0:
+                if board[row][col] == board[row+1][col+1] == \
+                    board[row + 2][col + 2] == board[row + 3][col + 3]:
+                    print("Player",board[row][col], "won!")
+                    return True
+
+def win():
+    if horizwin() or vertwin() or diagwin():
+        return True
     return False
 
-def cpu_move():
-    cpu_piece = 2
-    random_row = random.randint(0, 2)
-    random_column = random.randint(0, 2)
-    board[random_row][random_column] = cpu_piece
+
+def cpu_move(col,cpu_piece):
+    col = col - 1
+    for rows in range(num_row - 1, -1, -1):
+        if board[rows][col] == 0:
+            print("legal move")
+            board[rows][col] = cpu_piece
+            break
 
 
-
-
-
-
-
-
-while True:
+player = 1
+cpu_piece = 2
+while not win():
+    col = int(input("Enter a column: "))
+    stacking(col, player)
+    random_column = random.randint(0, 6)
+    cpu_move(random_column, cpu_piece)
     display_board()
-    player_move()
-    cpu_move()
-    if board == 1:
-        cpu_move()
-    if win_condition():
 
-        display_board()
-        break
+
+
+
+
+
+
+
 
 
 
